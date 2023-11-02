8ab9b0edf4a02af90b45f8538f3f0c6300bf8e77
CongWang5h <congwang.5h@gmail.com>
2023-11-02 13:42:37 -0400

    add review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,7 +25,6 @@ const authenticatedUser = (username, password) => { //returns boolean
 regd_users.post("/login", (req, res) => {
 	//Write your code here
 	// return res.status(300).json({ message: "Yet to be implemented" });
-	console.log('Received request:', req.body);
 	const username = req.body.username;
 	const password = req.body.password;
 
@@ -49,7 +48,20 @@ regd_users.post("/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
 	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	// return res.status(300).json({ message: "Yet to be implemented" });
+	const isbn = req.params.isbn;
+	const review = req.body.review;
+	const username = req.session.authorization.username;
+
+	if (isbn && review) {
+		if (!books[isbn]) {
+			return res.status(404).json({ message: "Book not found" });
+		}
+		books[isbn].reviews[username] = review;
+		return res.status(200).json({ message: "Review added successfully" });
+	} else {
+		return res.status(404).json({ message: "Error adding review" });
+	}
 });
 
 module.exports.authenticated = regd_users;

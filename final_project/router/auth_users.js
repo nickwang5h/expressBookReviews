46b059f16f3c801c2460b83ef2d31cd0ce055bb5
CongWang5h <congwang.5h@gmail.com>
2023-11-02 13:09:54 -0400

    login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -24,7 +24,25 @@ const authenticatedUser = (username, password) => { //returns boolean
 //only registered users can login
 regd_users.post("/login", (req, res) => {
 	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	// return res.status(300).json({ message: "Yet to be implemented" });
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (!username || !password) {
+		return res.status(404).json({ message: "Error logging in" });
+	}
+	if (authenticatedUser(username, password)) {
+		let accessToken = jwt.sign({ data: password }, "access", {
+			expiresIn: 60 * 60
+		});
+
+		req.session.authorization = {
+			accessToken, username
+		}
+		return res.status(200).json({ message: "User successfully logged in" });
+	} else {
+		return res.status(208).json({ message: "Invalid Login. Check username and password" });
+	}
 });
 
 // Add a book review

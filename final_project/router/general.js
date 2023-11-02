d6ca627deab44a40e3ec9c3b2a5f5762fa14eb2f
CongWang5h <congwang.5h@gmail.com>
2023-11-02 12:35:30 -0400

    book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,7 +60,9 @@ public_users.get('/title/:title', function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
 	//Write your code here
-	return res.status(300).json({ message: "Yet to be implemented" });
+	// return res.status(300).json({ message: "Yet to be implemented" });
+	let book = books[req.params.isbn];
+	res.send(book.reviews);
 });
 
 module.exports.general = public_users;
